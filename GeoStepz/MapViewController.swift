eb9e3a490a22c9b4e337c4a3614bca1caf2f782c
Abcarians, Areg <Areg.Abcarians@disney.com>
2016-02-04 21:06:54 -0800

    Initial map code working!

--- GeoStepz/MapViewController.swift
+++ GeoStepz/MapViewController.swift
@@ -10,28 +10,74 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController {
-    
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
     let regionRadius: CLLocationDistance = 1000
     
+    @IBOutlet weak var distanceReading: UILabel!
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        initLocation()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        view.backgroundColor = UIColor.grayColor()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    func initLocation() {
-        let initialLocation: CLLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
-        centerMapOnLocation(initialLocation)
+
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways {
+            if CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    //do stuff
+                    print("reached")
+                    startScanning()
+                }
+            }
+        }
     }
     
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
-        map.setRegion(coordinateRegion, animated: true)
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            let beacon = beacons[0]
+            updateDistance(beacon.proximity)
+        } else {
+            updateDistance(.Unknown)
+        }
     }
     
+    func startScanning() {
+        let uuid = NSUUID(UUIDString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoringForRegion(beaconRegion)
+        locationManager.startRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    func updateDistance(distance: CLProximity) {
+        UIView.animateWithDuration(0.8) { [unowned self] in
+            switch distance {
+            case .Unknown:
+                self.view.backgroundColor = UIColor.grayColor()
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .Far:
+                self.view.backgroundColor = UIColor.blueColor()
+                self.distanceReading.text = "FAR"
+                
+            case .Near:
+                self.view.backgroundColor = UIColor.orangeColor()
+                self.distanceReading.text = "NEAR"
+                
+            case .Immediate:
+                self.view.backgroundColor = UIColor.redColor()
+                self.distanceReading.text = "RIGHT HERE"
+            }
+        }
+    }
 }
