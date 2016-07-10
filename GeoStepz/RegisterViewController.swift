e269b95677f964020f91b67b9bfe8c4139965d95
Abcarians, Areg <Areg.Abcarians@disney.com>
2016-07-10 00:02:57 -0700

    Login/register switch now works (username: a, password: a), as well as some styling introduction

--- GeoStepz/RegisterViewController.swift
+++ GeoStepz/RegisterViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordReentryField: UITextField!
+    @IBAction func registerButton(sender: UIButton) {
+        if !register() {
+            let alert = UtilitiesHelper.getAlertInstance("Register Error", message: "Invalid registration information provided.", hasTextField: false, textFieldValue: "")
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+                print("OK clicked.")
+            }))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("MapViewController") as! MapViewController
+            self.presentViewController(secondViewController, animated: true, completion: nil)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        passwordField.secureTextEntry = true
+        passwordReentryField.secureTextEntry = true
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+    func register() -> Bool {
+        if emailField.text == "a" && usernameField.text == "a" && passwordField.text == "a" && passwordReentryField.text == "a" && passwordField.text == passwordReentryField.text {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
--- GeoStepz/SignInViewController.swift
+++ GeoStepz/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  GeoStepz
 //
 //  Created by Lee, Sang on 1/5/16.
@@ -9,10 +9,26 @@
 import UIKit
 
 class SignInViewController: UIViewController {
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func loginButton(sender: UIButton) {
+        if !logIn() {
+            let alert = UtilitiesHelper.getAlertInstance("Login Error", message: "Invalid username/password.", hasTextField: false, textFieldValue: "")
+
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+                print("OK clicked.")
+            }))
+
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            let secondViewController = self.storyboard?.instantiateViewControllerWithIdentifier("UITabBarControllerMain") as! UITabBarController
+            self.presentViewController(secondViewController, animated: true, completion: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        passwordField.secureTextEntry = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,12 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func logIn() -> Bool {
+        if emailField.text == "a" && passwordField.text == "a" {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
