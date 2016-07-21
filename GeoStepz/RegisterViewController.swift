ae7a03cfa29d6bb838ddb3f78a77afe3185c5a3a
Sang Lee <sang.lee@disney.com>
2016-07-20 17:18:27 -0700

    photos

--- GeoStepz/RegisterViewController.swift
+++ GeoStepz/RegisterViewController.swift
@@ -7,19 +7,114 @@
 //
 
 import UIKit
+import Photos
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var images:NSMutableArray! // <-- Array to hold the fetched images
+    var totalImageCountNeeded:Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
+    @IBOutlet weak var imagePicked: UIImageView!
     
+    @IBAction func testCamera(sender: AnyObject) {
+        print("works")
+        showPhotos()
+    }
+    
+    func showPhotos() {
+        print("show photos")
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePicker.allowsEditing = true
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+        
+        fetchPhotos ()
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let imageUrl = info["UIImagePickerControllerReferenceURL"] as! NSURL
+            let asset = PHAsset.fetchAssetsWithALAssetURLs([imageUrl], options: nil).firstObject as! PHAsset
+            print("location: " + String(asset.location))
+            print("Creation Date: " + String(asset.creationDate))
+            imagePicked.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func fetchPhotos () {
+        images = NSMutableArray()
+        totalImageCountNeeded = 3
+        self.fetchPhotoAtIndexFromEnd(0)
+    }
+    
+    func fetchPhotoAtIndexFromEnd(index:Int) {
+        
+        let imgManager = PHImageManager.defaultManager()
+        
+        // Note that if the request is not set to synchronous
+        // the requestImageForAsset will return both the image
+        // and thumbnail; by setting synchronous to true it
+        // will return just the thumbnail
+        var requestOptions = PHImageRequestOptions()
+        requestOptions.synchronous = true
+        
+        // Sort the images by creation date
+        var fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key:"creationDate", ascending: true)]
+        
+        if let fetchResult: PHFetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions) {
+            
+            // If the fetch result isn't empty,
+            // proceed with the image request
+            if fetchResult.count > 0 {
+                // Perform the image request
+                imgManager.requestImageForAsset(fetchResult.objectAtIndex(fetchResult.count - 1 - index) as! PHAsset, targetSize: view.frame.size, contentMode: PHImageContentMode.AspectFill, options: requestOptions, resultHandler: { (image, _) in
+                    
+                    // Add the returned image to your array
+                    self.images.addObject(image!)
+                    
+                    // If you haven't already reached the first
+                    // index of the fetch result and if you haven't
+                    // already stored all of the images you need,
+                    // perform the fetch request again with an
+                    // incremented index
+                    if index + 1 < fetchResult.count && self.images.count < self.totalImageCountNeeded {
+                        self.fetchPhotoAtIndexFromEnd(index + 1)
+                    } else {
+                        // Else you have completed creating your array
+                        print("Completed array: \(self.images)")
+                    }
+                })
+            }
+        }
+    }
+    
+    func saveButt(sender: AnyObject) {
+        var imageData = UIImageJPEGRepresentation(imagePicked.image!, 0.6)
+        var compressedJPGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
+        
+        var alert = UIAlertView(title: "Wow",
+            message: "Your image has been saved to Photo Library!",
+            delegate: nil,
+            cancelButtonTitle: "Ok")
+        
+        alert.show()
+    }
 }
 
